ae2196b1ec75596519f6915fbbeb97a1568aed83
Syrent <Syrent2356@gmail.com>
2022-10-04 06:50:37 +0330

    Change velocity startup messages.

--- src/main/java/ir/syrent/velocityreport/VelocityReport.kt
+++ src/main/java/ir/syrent/velocityreport/VelocityReport.kt
@@ -16,10 +16,6 @@ import me.mohamad82.ruom.VRUoMPlugin
 import me.mohamad82.ruom.VRuom
 import me.mohamad82.ruom.messaging.VelocityMessagingEvent
 import me.mohamad82.ruom.utils.MilliCounter
-import net.kyori.adventure.audience.Audience
-import net.kyori.adventure.audience.MessageType
-import net.kyori.adventure.text.ComponentLike
-import net.kyori.adventure.text.minimessage.MiniMessage
 import org.slf4j.Logger
 import java.util.*
 
@@ -33,7 +29,7 @@ class VelocityReport @Inject constructor(server: ProxyServer, logger: Logger) : 
     fun onProxyInitialization(event: ProxyInitializeEvent) {
         initializeMessagingChannels()
         initializeListeners()
-        sendFiglet()
+        sendMessages()
     }
 
     private fun initializeMessagingChannels() {
@@ -53,24 +49,10 @@ class VelocityReport @Inject constructor(server: ProxyServer, logger: Logger) : 
         ServerConnectedListener(this)
     }
 
-    private fun sendFiglet() {
-        log(" ")
-        log("<dark_red>__     __   _            _ _         ____                       _   ")
-        log("<dark_red>\\ \\   / /__| | ___   ___(_) |_ _   _|  _ \\ ___ _ __   ___  _ __| |_ ")
-        log("<dark_red> \\ \\ / / _ \\ |/ _ \\ / __| | __| | | | |_) / _ \\ '_ \\ / _ \\| '__| __|")
-        log("<dark_red>  \\ V /  __/ | (_) | (__| | |_| |_| |  _ <  __/ |_) | (_) | |  | |_ ")
-        log("<dark_red>   \\_/ \\___|_|\\___/ \\___|_|\\__|\\__, |_| \\_\\___| .__/ \\___/|_|   \\__|")
-        log("<dark_red>                               |___/          |_|                   v${getServer().pluginManager.getPlugin("velocityreport").get().description.version.get()}")
-        log(" ")
-        log("<yellow>You should install VelocityReport on your backend servers (Spigot, Paper, Purpur, etc...) as well.")
-        log("<yellow>To sync reports data between servers you have to use MySQL as database method.")
-        log(" ")
-        log("<white>Wiki: <blue><u>https://github.com/Syrent/VelocityReport/wiki</u></blue>")
-        log(" ")
-    }
-
-    private fun log(message: String) {
-        getServer().sendMessage(MiniMessage.miniMessage().deserialize("<gray>$message"))
+    private fun sendMessages() {
+        VRuom.log("You should install VelocityReport on your backend servers (Spigot, Paper, Purpur, etc...) as well.")
+        VRuom.log("To sync reports data between servers you have to use MySQL as database method.")
+        VRuom.log("Wiki: https://github.com/Syrent/VelocityReport/wiki")
     }
 
 }
