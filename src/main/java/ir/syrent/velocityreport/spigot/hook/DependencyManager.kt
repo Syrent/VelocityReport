dfd2e00c315b349ff1e660edaec78196a29d9a94
Syrent <56670179+Syrent@users.noreply.github.com>
2023-01-02 02:46:53 +0330

    Spigot | Add `%velocityreport_reports_[stage]%` placeholder

--- src/main/java/ir/syrent/velocityreport/spigot/hook/DependencyManager.kt
+++ src/main/java/ir/syrent/velocityreport/spigot/hook/DependencyManager.kt
@@ -4,11 +4,17 @@ object DependencyManager {
 
     var velocityVanishHook: VelocityVanishHook
         private set
+    var placeholderAPIHook: PlaceholderAPIHook
+        private set
 
     init {
         VelocityVanishHook("VelocityVanish").apply {
             this.register()
             velocityVanishHook = this
+        }
+        PlaceholderAPIHook(VelocityVanishSpigot.instance, "PlaceholderAPI").apply {
+            this.register()
+            placeholderAPIHook = this
         }
     }
 
