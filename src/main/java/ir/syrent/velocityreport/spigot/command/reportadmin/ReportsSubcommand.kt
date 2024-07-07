654f1aad6685863969ee811c5a39aa8fab28c84d
Syrent <Syrent2356@Gmail.com>
2024-07-07 18:18:14 +0330

    chore: update all dependencies to get ready for 1.21

--- src/main/java/ir/syrent/velocityreport/spigot/command/reportadmin/ReportsSubcommand.kt
+++ src/main/java/ir/syrent/velocityreport/spigot/command/reportadmin/ReportsSubcommand.kt
@@ -1,15 +1,16 @@
 package ir.syrent.velocityreport.spigot.command.reportadmin
 
 import ir.syrent.velocityreport.report.ReportStage
-import ir.syrent.velocityreport.spigot.Ruom
 import ir.syrent.velocityreport.spigot.adventure.ComponentUtils
 import ir.syrent.velocityreport.spigot.command.library.SubCommand
 import ir.syrent.velocityreport.spigot.storage.Database
 import ir.syrent.velocityreport.spigot.storage.Message
 import ir.syrent.velocityreport.spigot.storage.Settings
-import ir.syrent.velocityreport.utils.*
+import ir.syrent.velocityreport.utils.TextReplacement
+import ir.syrent.velocityreport.utils.component
+import ir.syrent.velocityreport.utils.openBook
+import ir.syrent.velocityreport.utils.sendMessage
 import net.kyori.adventure.inventory.Book
-import net.kyori.adventure.platform.bukkit.BukkitAudiences
 import net.kyori.adventure.text.Component
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
@@ -25,35 +26,39 @@ class ReportsSubcommand : SubCommand("reports", "velocityreport.admin.reports", 
         sender.sendMessage(Message.REPORTADMIN_REPORTS_RECEIVE)
 
         Database.getReports(ReportStage.ACTIVE).whenComplete { reports, _ ->
-            val pages = mutableListOf<Component>()
-            val header = Settings.reportsBookHeader
-            var lineCount = 0
+            try {
+                val pages = mutableListOf<Component>()
+                val header = Settings.reportsBookHeader
+                var lineCount = 0
 
-            pageLines -= header.size
+                pageLines -= header.size
 
-            val page = StringBuilder()
+                val page = StringBuilder()
 
-            page.append(Settings.formatMessage(header).joinToString("\n")).append("\n")
-            for (report in reports) {
-                if (lineCount == pageLines) {
-                    pages.add(page.toString().component())
-                    page.clear()
-                    page.append(Settings.formatMessage(header).joinToString("\n")).append("\n")
-                    lineCount = 0
+                page.append(Settings.formatMessage(header).joinToString("\n")).append("\n")
+                for (report in reports) {
+                    if (lineCount == pageLines) {
+                        pages.add(page.toString().component())
+                        page.clear()
+                        page.append(Settings.formatMessage(header).joinToString("\n")).append("\n")
+                        lineCount = 0
+                    }
+                    page.append(Settings.formatMessage(
+                        Message.REPORTADMIN_REPORTS_FORMAT,
+                        TextReplacement("reporter", report.reporterName),
+                        TextReplacement("reported", report.reportedName),
+                        TextReplacement("server", report.server),
+                        TextReplacement("reason", report.reason),
+                        TextReplacement("id", report.reportID.toString())
+                    ).replace("\\n", "\n")).append("\n")
+                    lineCount++
                 }
-                page.append(Settings.formatMessage(
-                    Message.REPORTADMIN_REPORTS_FORMAT,
-                    TextReplacement("reporter", report.reporterName),
-                    TextReplacement("reported", report.reportedName),
-                    TextReplacement("server", report.server),
-                    TextReplacement("reason", report.reason),
-                    TextReplacement("id", report.reportID.toString())
-                ).replace("\\n", "\n")).append("\n")
-                lineCount++
-            }
-            pages.add(page.toString().component())
+                pages.add(page.toString().component())
 
-            sender.openBook(Book.book(title, title, pages))
+                sender.openBook(Book.book(title, title, pages))
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
     }
 }
