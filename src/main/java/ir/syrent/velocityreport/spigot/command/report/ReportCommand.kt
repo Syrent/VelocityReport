a6467ae5307398d2e7a818f3cc3c6d08d5e05130
syrent <syrent2356@gmail.com>
2024-10-05 09:08:24 +0330

    fix: cross-server reports

--- src/main/java/ir/syrent/velocityreport/spigot/command/report/ReportCommand.kt
+++ src/main/java/ir/syrent/velocityreport/spigot/command/report/ReportCommand.kt
@@ -57,7 +57,7 @@ class ReportCommand(
             }
         }
 
-        if (target == null || (!Bukkit.getOfflinePlayer(target).hasPlayedBefore() && Bukkit.getOfflinePlayer(target).player == null) || !BukkitVanishUtils.canSee(sender as? Player, Bukkit.getOfflinePlayer(target).uniqueId)) {
+        if (target == null || (!Bukkit.getOfflinePlayer(target).hasPlayedBefore() && (!Settings.velocitySupport && Bukkit.getOfflinePlayer(target).player == null)) || !BukkitVanishUtils.canSee(sender as? Player, Bukkit.getOfflinePlayer(target).uniqueId)) {
             sender.sendMessage(Message.NO_TARGET)
             return
         }
